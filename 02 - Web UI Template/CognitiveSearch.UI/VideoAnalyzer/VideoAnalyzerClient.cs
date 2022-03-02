e7634dc066ca921ca61cee746f781ff10c6ae9e7
ruoccofabrizio <fruocco@fruocco>
2022-03-02 09:16:06 +0100

    added support for ARM-based AVAM accounts

--- 02 - Web UI Template/CognitiveSearch.UI/VideoAnalyzer/VideoAnalyzerClient.cs
+++ 02 - Web UI Template/CognitiveSearch.UI/VideoAnalyzer/VideoAnalyzerClient.cs
@@ -1,30 +1,25 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure;
 using Azure.Core;
-using Azure.Core.Pipeline;
-using Azure.Search.Documents;
-using Azure.Search.Documents.Indexes;
-using Azure.Search.Documents.Indexes.Models;
-using Azure.Search.Documents.Models;
-using Azure.Storage;
-using Azure.Storage.Blobs;
-using Azure.Storage.Sas;
-using Microsoft.ApplicationInsights;
+using Azure.Identity;
+
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Security.Authentication;
-using System.Text.Json.Serialization;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CognitiveSearch.UI
 {
+    public class VideoAnalyzerAccessToken
+    {
+        public string AccessToken { get; set; }
+
+    }
     public class VideoAnalyzerClient
     {
         private IConfiguration _configuration { get; set; }
@@ -33,7 +28,7 @@ namespace CognitiveSearch.UI
         private string apiKey { get; set; }
         private string accountId { get; set; }
         private string accountLocation { get; set; }
-      
+        private string resourceId { get; set; }
 
         public VideoAnalyzerClient(IConfiguration configuration)
         {
@@ -45,6 +40,7 @@ namespace CognitiveSearch.UI
                 apiKey = _configuration.GetSection("AVAM_Api_Key")?.Value;
                 accountId = _configuration.GetSection("AVAM_Account_Id")?.Value;
                 accountLocation = _configuration.GetSection("AVAM_Account_Location")?.Value;
+                resourceId = _configuration.GetSection("AVAM_Resource_Id")?.Value;
 
             }
             catch (Exception e)
@@ -63,16 +59,42 @@ namespace CognitiveSearch.UI
             httpHandler.SslOptions.EnabledSslProtocols |= SslProtocols.Tls12;
             var client = new HttpClient(httpHandler);
 
-            var getAccountsRequest = new HttpRequestMessage(HttpMethod.Get, $"{apiUrl}/auth/{accountLocation}/Accounts/{accountId}/Videos/{videoId}/AccessToken");
-            getAccountsRequest.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
-            getAccountsRequest.Headers.Add("x-ms-client-request-id", Guid.NewGuid().ToString()); // Log the request id so you can include it in case you wish to report an issue for API errors or unexpected API behavior
-            var result = await client.SendAsync(getAccountsRequest);
-            Console.WriteLine("Response id to log: " + result.Headers.GetValues("x-ms-request-id").FirstOrDefault()); // Log the response id so you can include it in case you wish to report an issue for API errors or unexpected API behavior
-            string accessToken = (await result.Content.ReadAsStringAsync()).Trim('"'); // The access token is returned as JSON value surrounded by double-quotes
+            if (apiKey == null || apiKey == "")
+            {
+                // Use AzureCredential to generate Azure Video Analyzer for Media Access Token
+                var credential = new ChainedTokenCredential(new ManagedIdentityCredential(), new DefaultAzureCredential());
 
-            accessToken = $"https://www.videoindexer.ai/embed/player/{accountId}/{videoId}?location={accountLocation}&accessToken={accessToken}";        
+                string[] scopes = new string[1];
+                scopes[0] = "https://management.azure.com/.default";
+                var access_token = credential.GetToken(new TokenRequestContext(scopes));
+                Console.WriteLine(access_token.Token.ToString());
+                string url = $"https://management.azure.com{resourceId}/generateAccessToken?api-version=2021-10-18-preview";
 
-            return accessToken;
+                var getAccountsRequest = new HttpRequestMessage(HttpMethod.Post, url);
+                getAccountsRequest.Headers.Add("Authorization", $"Bearer {access_token.Token.ToString()}");
+                getAccountsRequest.Content = new StringContent("{\"permissionType\":\"Contributor\",\"scope\": \"Account\"}", Encoding.UTF8, "application/json");
+                var result = await client.SendAsync(getAccountsRequest);
+                string accessToken = await result.Content.ReadAsStringAsync();
+                var accessTokenObj = JsonConvert.DeserializeObject<VideoAnalyzerAccessToken>(accessToken);
+                accessToken = $"https://www.videoindexer.ai/embed/player/{accountId}/{videoId}?location={accountLocation}&accessToken={accessTokenObj.AccessToken}";
+
+                return accessToken;
+            }
+
+            else
+            {
+                // Use API Key for Video Indexer (or not ARM Azure Video Analyzer for Media)
+                var getAccountsRequest = new HttpRequestMessage(HttpMethod.Get, $"{apiUrl}/auth/{accountLocation}/Accounts/{accountId}/Videos/{videoId}/AccessToken");
+                getAccountsRequest.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+                getAccountsRequest.Headers.Add("x-ms-client-request-id", Guid.NewGuid().ToString()); // Log the request id so you can include it in case you wish to report an issue for API errors or unexpected API behavior
+                var result = await client.SendAsync(getAccountsRequest);
+                Console.WriteLine("Response id to log: " + result.Headers.GetValues("x-ms-request-id").FirstOrDefault()); // Log the response id so you can include it in case you wish to report an issue for API errors or unexpected API behavior
+                string accessToken = (await result.Content.ReadAsStringAsync()).Trim('"'); // The access token is returned as JSON value surrounded by double-quotes
+
+                accessToken = $"https://www.videoindexer.ai/embed/player/{accountId}/{videoId}?location={accountLocation}&accessToken={accessToken}";
+
+                return accessToken;
+            }
         }
     }
 }
